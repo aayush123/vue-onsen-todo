0b18ec42544dccaf0185657f8856efd817950487
Ayush Gupta <aayush123@gmail.com>
2017-09-22 07:45:15 +0530

    updated deletion logic and added app icon. pushing build files and app icon image

--- src/store/mutations/DeleteAllCompletedTasksMutation.js
+++ src/store/mutations/DeleteAllCompletedTasksMutation.js
@@ -3,7 +3,7 @@ import Vue from 'vue';
 export default function (state) {
   const newTasksArr = [];
   for (let tasksIdx = 0; tasksIdx < state.tasks.length; tasksIdx += 1) {
-    if (!state.tasks[tasksIdx].completed) {
+    if (!state.tasks[tasksIdx].completed || state.tasks[tasksIdx].projectName !== state.activeProject) {
       newTasksArr.push(state.tasks[tasksIdx]);
     }
   }
